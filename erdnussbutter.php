c5ca69e9b02cadfa6311d0eacd3e1953974b261d
marco.druschba@uni-oldenburg.de <marco.druschba@uni-oldenburg.de>
2022-08-14 21:47:45 +0200

    added first 12 erdnussbutter products to db

--- erdnussbutter.php
+++ erdnussbutter.php
@@ -12,7 +12,7 @@
       $db = new PDO('sqlite:db/produkte.db');
 
       // Select 12 random entries of DB
-      $sql = 'SELECT * FROM erdnussbutter ORDER BY RANDOM() LIMIT 12';
+      $sql = 'SELECT DISTINCT * FROM erdnussbutter WHERE price IS NOT NULL ORDER BY RANDOM() LIMIT 12';
       $result = $db->prepare($sql);
       $result->execute();
 
@@ -93,103 +93,103 @@
 
     <!-- display 12 random products -->
     <div class="small-container">
-      <h2>Erdnussbutter-Produkte</h2>
+      <h1>Erdnussbutter-Produkte:</h1>
       <div class="row">
         <div class="col-4">
-          <img src="images/test.png" alt="product">
+        <img src="<?php echo "{$rows[0]['picture_path']}";?>" alt="product-1">
           <h4><?php echo "{$rows[0]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[0]['price']}"."€";?></p>
-          <?php printNutriscore(0); ?><br>
+          <p><?php echo "{$rows[0]['price']}"."€ pro 500g";?></p>
+          <?php printNutriscore(0); ?>
           <?php printEcoscore(0); ?>
-          <button class='btn add'>Hinzufügen</button>
+          <button class='btn add'>Zum Warenkorb hinzufügen</button>
         </div>
         <div class="col-4">
-          <img src="images/test.png" alt="product">
+          <img src="<?php echo "{$rows[1]['picture_path']}";?>" alt="product-2">
           <h4><?php echo "{$rows[1]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[1]['price']}"."€";?></p>
-          <?php printNutriscore(1); ?><br>
+          <p><?php echo "{$rows[1]['price']}"."€ pro 500g";?></p>
+          <?php printNutriscore(1); ?>
           <?php printEcoscore(1); ?>
-          <button class='btn add'>Hinzufügen</button>
+          <button class='btn add'>Zum Warenkorb hinzufügen</button>
         </div>
         <div class="col-4">
-          <img src="images/test.png" alt="product">
+          <img src="<?php echo "{$rows[2]['picture_path']}";?>" alt="product-3">
           <h4><?php echo "{$rows[2]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[2]['price']}"."€";?></p>
-          <?php printNutriscore(2); ?><br>
+          <p><?php echo "{$rows[2]['price']}"."€ pro 500g";?></p>
+          <?php printNutriscore(2); ?>
           <?php printEcoscore(2); ?>
-          <button class='btn add'>Hinzufügen</button>
+          <button class='btn add'>Zum Warenkorb hinzufügen</button>
         </div>
         <div class="col-4">
-          <img src="images/test.png" alt="product">
+          <img src="<?php echo "{$rows[3]['picture_path']}";?>" alt="product-4">
           <h4><?php echo "{$rows[3]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[3]['price']}"."€";?></p>
-          <?php printNutriscore(3); ?><br>
+          <p><?php echo "{$rows[3]['price']}"."€ pro 500g";?></p>
+          <?php printNutriscore(3); ?>
           <?php printEcoscore(3); ?>
-          <button class='btn add'>Hinzufügen</button>
+          <button class='btn add'>Zum Warenkorb hinzufügen</button>
         </div>
         <div class="col-4">
-          <img src="images/test.png" alt="product">
+          <img src="<?php echo "{$rows[4]['picture_path']}";?>" alt="product-5">
           <h4><?php echo "{$rows[4]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[4]['price']}"."€";?></p>
-          <?php printNutriscore(4); ?><br>
+          <p><?php echo "{$rows[4]['price']}"."€ pro 500g";?></p>
+          <?php printNutriscore(4); ?>
           <?php printEcoscore(4); ?>
-          <button class='btn add'>Hinzufügen</button>
+          <button class='btn add'>Zum Warenkorb hinzufügen</button>
         </div>
         <div class="col-4">
-          <img src="images/test.png" alt="product">
+          <img src="<?php echo "{$rows[5]['picture_path']}";?>" alt="product-6">
           <h4><?php echo "{$rows[5]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[5]['price']}"."€";?></p>
-          <?php printNutriscore(5); ?><br>
+          <p><?php echo "{$rows[5]['price']}"."€ pro 500g";?></p>
+          <?php printNutriscore(5); ?>
           <?php printEcoscore(5); ?>
-          <button class='btn add'>Hinzufügen</button>
+          <button class='btn add'>Zum Warenkorb hinzufügen</button>
         </div>
         <div class="col-4">
-          <img src="images/test.png" alt="product">
+          <img src="<?php echo "{$rows[6]['picture_path']}";?>" alt="product-7">
           <h4><?php echo "{$rows[6]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[6]['price']}"."€";?></p>
-          <?php printNutriscore(6); ?><br>
+          <p><?php echo "{$rows[6]['price']}"."€ pro 500g";?></p>
+          <?php printNutriscore(6); ?>
           <?php printEcoscore(6); ?>
-          <button class='btn add'>Hinzufügen</button>
+          <button class='btn add'>Zum Warenkorb hinzufügen</button>
         </div>
         <div class="col-4">
-          <img src="images/test.png" alt="product">
+          <img src="<?php echo "{$rows[7]['picture_path']}";?>" alt="product-8">
           <h4><?php echo "{$rows[7]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[7]['price']}"."€";?></p>
-          <?php printNutriscore(7); ?><br>
+          <p><?php echo "{$rows[7]['price']}"."€ pro 500g";?></p>
+          <?php printNutriscore(7); ?>
           <?php printEcoscore(7); ?>
-          <button class='btn add'>Hinzufügen</button>
+          <button class='btn add'>Zum Warenkorb hinzufügen</button>
         </div>
         <div class="col-4">
-          <img src="images/test.png" alt="product">
+          <img src="<?php echo "{$rows[8]['picture_path']}";?>" alt="product-9">
           <h4><?php echo "{$rows[8]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[8]['price']}"."€";?></p>
-          <?php printNutriscore(8); ?><br>
+          <p><?php echo "{$rows[8]['price']}"."€ pro 500g";?></p>
+          <?php printNutriscore(8); ?>
           <?php printEcoscore(8); ?>
-          <button class='btn add'>Hinzufügen</button>
+          <button class='btn add'>Zum Warenkorb hinzufügen</button>
         </div>
         <div class="col-4">
-          <img src="images/test.png" alt="product">
+          <img src="<?php echo "{$rows[9]['picture_path']}";?>" alt="product-10">
           <h4><?php echo "{$rows[9]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[9]['price']}"."€";?></p>
-          <?php printNutriscore(9); ?><br>
+          <p><?php echo "{$rows[9]['price']}"."€ pro 500g";?></p>
+          <?php printNutriscore(9); ?>
           <?php printEcoscore(9); ?>
-          <button class='btn add'>Hinzufügen</button>
+          <button class='btn add'>Zum Warenkorb hinzufügen</button>
         </div>
         <div class="col-4">
-          <img src="images/test.png" alt="product">
+          <img src="<?php echo "{$rows[10]['picture_path']}";?>" alt="product-11">
           <h4><?php echo "{$rows[10]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[10]['price']}"."€";?></p>
-          <?php printNutriscore(10); ?><br>
+          <p><?php echo "{$rows[10]['price']}"."€ pro 500g";?></p>
+          <?php printNutriscore(10); ?>
           <?php printEcoscore(10); ?>
-          <button class='btn add'>Hinzufügen</button>
+          <button class='btn add'>Zum Warenkorb hinzufügen</button>
         </div>
         <div class="col-4">
-          <img src="images/test.png" alt="product">
+          <img src="<?php echo "{$rows[11]['picture_path']}";?>" alt="product-12">
           <h4><?php echo "{$rows[11]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[11]['price']}"."€";?></p>
-          <?php printNutriscore(11); ?><br>
+          <p><?php echo "{$rows[11]['price']}"."€ pro 500g";?></p>
+          <?php printNutriscore(11); ?>
           <?php printEcoscore(11); ?>
-          <button class='btn add'>Hinzufügen</button>
+          <button class='btn add'>Zum Warenkorb hinzufügen</button>
         </div>
       </div>
 
