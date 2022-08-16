367e60abd0a0ac875b38ab1ae44513113ee163ed
marco.druschba@uni-oldenburg.de <marco.druschba@uni-oldenburg.de>
2022-08-17 00:35:55 +0200

    added all 36 milch products to db

--- milch.php
+++ milch.php
@@ -3,7 +3,7 @@
 <html lang="de">
   <head>
     <?php include "includes/meta.php" ?>
-    <title>Groceries | Milch</title>
+    <title>Groceries | Milch & -ersatz</title>
   </head>
 
   <?php
@@ -12,7 +12,7 @@
       $db = new PDO('sqlite:db/produkte.db');
 
       // Select 12 random entries of DB
-      $sql = 'SELECT DISTINCT * FROM popcorn ORDER BY RANDOM() LIMIT 12';
+      $sql = 'SELECT DISTINCT * FROM milch ORDER BY RANDOM() LIMIT 12';
       $result = $db->prepare($sql);
       $result->execute();
 
@@ -93,12 +93,12 @@
 
     <!-- display 12 random products -->
     <div class="small-container">
-      <h1>Milch</h1>
+      <h1>Milch & -ersatz</h1>
       <div class="row">
         <div class="col-4">
         <img src="<?php echo "{$rows[0]['picture_path']}";?>" alt="product-1">
           <h4><?php echo "{$rows[0]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[0]['price']}"."€ pro 500g";?></p>
+          <p><?php echo "{$rows[0]['price']}"."€ pro 1L";?></p>
           <?php printNutriscore(0); ?>
           <?php printEcoscore(0); ?>
           <button class='btn add'>Zum Warenkorb hinzufügen</button>
@@ -106,7 +106,7 @@
         <div class="col-4">
           <img src="<?php echo "{$rows[1]['picture_path']}";?>" alt="product-2">
           <h4><?php echo "{$rows[1]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[1]['price']}"."€ pro 500g";?></p>
+          <p><?php echo "{$rows[1]['price']}"."€ pro 1L";?></p>
           <?php printNutriscore(1); ?>
           <?php printEcoscore(1); ?>
           <button class='btn add'>Zum Warenkorb hinzufügen</button>
@@ -114,7 +114,7 @@
         <div class="col-4">
           <img src="<?php echo "{$rows[2]['picture_path']}";?>" alt="product-3">
           <h4><?php echo "{$rows[2]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[2]['price']}"."€ pro 500g";?></p>
+          <p><?php echo "{$rows[2]['price']}"."€ pro 1L";?></p>
           <?php printNutriscore(2); ?>
           <?php printEcoscore(2); ?>
           <button class='btn add'>Zum Warenkorb hinzufügen</button>
@@ -122,7 +122,7 @@
         <div class="col-4">
           <img src="<?php echo "{$rows[3]['picture_path']}";?>" alt="product-4">
           <h4><?php echo "{$rows[3]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[3]['price']}"."€ pro 500g";?></p>
+          <p><?php echo "{$rows[3]['price']}"."€ pro 1L";?></p>
           <?php printNutriscore(3); ?>
           <?php printEcoscore(3); ?>
           <button class='btn add'>Zum Warenkorb hinzufügen</button>
@@ -130,7 +130,7 @@
         <div class="col-4">
           <img src="<?php echo "{$rows[4]['picture_path']}";?>" alt="product-5">
           <h4><?php echo "{$rows[4]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[4]['price']}"."€ pro 500g";?></p>
+          <p><?php echo "{$rows[4]['price']}"."€ pro 1L";?></p>
           <?php printNutriscore(4); ?>
           <?php printEcoscore(4); ?>
           <button class='btn add'>Zum Warenkorb hinzufügen</button>
@@ -138,7 +138,7 @@
         <div class="col-4">
           <img src="<?php echo "{$rows[5]['picture_path']}";?>" alt="product-6">
           <h4><?php echo "{$rows[5]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[5]['price']}"."€ pro 500g";?></p>
+          <p><?php echo "{$rows[5]['price']}"."€ pro 1L";?></p>
           <?php printNutriscore(5); ?>
           <?php printEcoscore(5); ?>
           <button class='btn add'>Zum Warenkorb hinzufügen</button>
@@ -146,7 +146,7 @@
         <div class="col-4">
           <img src="<?php echo "{$rows[6]['picture_path']}";?>" alt="product-7">
           <h4><?php echo "{$rows[6]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[6]['price']}"."€ pro 500g";?></p>
+          <p><?php echo "{$rows[6]['price']}"."€ pro 1L";?></p>
           <?php printNutriscore(6); ?>
           <?php printEcoscore(6); ?>
           <button class='btn add'>Zum Warenkorb hinzufügen</button>
@@ -154,7 +154,7 @@
         <div class="col-4">
           <img src="<?php echo "{$rows[7]['picture_path']}";?>" alt="product-8">
           <h4><?php echo "{$rows[7]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[7]['price']}"."€ pro 500g";?></p>
+          <p><?php echo "{$rows[7]['price']}"."€ pro 1L";?></p>
           <?php printNutriscore(7); ?>
           <?php printEcoscore(7); ?>
           <button class='btn add'>Zum Warenkorb hinzufügen</button>
@@ -162,7 +162,7 @@
         <div class="col-4">
           <img src="<?php echo "{$rows[8]['picture_path']}";?>" alt="product-9">
           <h4><?php echo "{$rows[8]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[8]['price']}"."€ pro 500g";?></p>
+          <p><?php echo "{$rows[8]['price']}"."€ pro 1L";?></p>
           <?php printNutriscore(8); ?>
           <?php printEcoscore(8); ?>
           <button class='btn add'>Zum Warenkorb hinzufügen</button>
@@ -170,7 +170,7 @@
         <div class="col-4">
           <img src="<?php echo "{$rows[9]['picture_path']}";?>" alt="product-10">
           <h4><?php echo "{$rows[9]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[9]['price']}"."€ pro 500g";?></p>
+          <p><?php echo "{$rows[9]['price']}"."€ pro 1L";?></p>
           <?php printNutriscore(9); ?>
           <?php printEcoscore(9); ?>
           <button class='btn add'>Zum Warenkorb hinzufügen</button>
@@ -178,7 +178,7 @@
         <div class="col-4">
           <img src="<?php echo "{$rows[10]['picture_path']}";?>" alt="product-11">
           <h4><?php echo "{$rows[10]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[10]['price']}"."€ pro 500g";?></p>
+          <p><?php echo "{$rows[10]['price']}"."€ pro 1L";?></p>
           <?php printNutriscore(10); ?>
           <?php printEcoscore(10); ?>
           <button class='btn add'>Zum Warenkorb hinzufügen</button>
@@ -186,7 +186,7 @@
         <div class="col-4">
           <img src="<?php echo "{$rows[11]['picture_path']}";?>" alt="product-12">
           <h4><?php echo "{$rows[11]['product_name_de']}";?></h4>
-          <p><?php echo "{$rows[11]['price']}"."€ pro 500g";?></p>
+          <p><?php echo "{$rows[11]['price']}"."€ pro 1L";?></p>
           <?php printNutriscore(11); ?>
           <?php printEcoscore(11); ?>
           <button class='btn add'>Zum Warenkorb hinzufügen</button>
