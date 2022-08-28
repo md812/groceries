4c9d7e11bc73964ea2d69e2cf07f5b45370bdf85
marco.druschba@uni-oldenburg.de <marco.druschba@uni-oldenburg.de>
2022-08-29 00:56:00 +0200

    minor fix

--- includes/nav.php
+++ includes/nav.php
@@ -1,7 +1,7 @@
 <div class="navbar">
   <!-- logo, image source: picture by Prawny on Pixabay (https://pixabay.com/de/users/prawny-162579/) -->
   <div class="logo">
-    <a href="index.php"><img src="images/logo.png" width="125px" alt="groceries-logo">
+    <a href="index.php"><img src="images/logo.png" id="logo" alt="groceries-logo">
       <h1>Groceries</h1>
     </a>
   </div>
